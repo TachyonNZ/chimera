b27174cc3b9c91ea92b4cbdf9ad665fc811dcc9f
Micah <micah@tachyonnz.com>
2016-11-01 18:13:07 +1300

    Added source files
    Uses YAML, so you'd better have them if you want to try to run the program.

---
+++ test.py
@@ -0,0 +1,534 @@
+import yaml, random, argparse
+try:
+    from yaml import CLoader as Loader, CDumper as Dumper
+except ImportError:
+    from yaml import Loader, Dumper
+
+with open('test.yaml', 'r') as file:
+	raw_dict = yaml.load(file, Loader=Loader)
+
+already_changed = []
+parts = []
+animals = []
+activities = []
+furLocations = []
+robotstring = ""
+
+nontf = ["natural oddity","closet monster","freak of nature","synthetic being","cybernetically enhanced being","organic robot","alien","eldritch minor god","incarnated spirit","child's imaginary friend"]
+synth = ["synthetic being","cybernetically enhanced being","organic robot"]
+magic = ["magic experiment","eldritch minor god","incarnated spirit","wizard's minion","reincarnated being","victim of a mischevious spirit",]
+
+
+def choose(category,dict):
+    if category in dict:
+        choice = random.choice(dict.get(category))
+    else:
+        for k,v in dict.items():
+            choice = random.choice(v)
+			# This is a mess, but it's the only way I could find to
+			# grab the array out without knowing the name of the key.
+    if type(choice) is type({}) or type(choice) is type([]):
+		# this is also a horrible mess. Doing it the logical way
+		# 		if type(choice) is dict or type(choice) is array
+		# doesn't seem to work for some reason
+        return choose("",choice)
+    else:
+        return choice
+
+def generateRobotParts():
+    robocations = []
+    robotype = []
+    robs = choose("fur_locations",raw_dict)
+    for i in range(random.randrange(2,5)):
+        while robs in robocations:
+            robs = choose("fur_locations",raw_dict)
+        robocations.append(robs)
+
+    for i in range(len(robocations)):
+        robotype.append(choose("robot_descriptors",raw_dict))
+    if "entire body" in furLocations:
+        robocations = ["entire body"]
+    if "none" in robocations:
+        robocations.remove("none")
+    return robocations,robotype
+
+familyname = ""
+
+def nameGen(intelligence,gender,family):
+    final = ""
+    partOne = ""
+    partTwo = ""
+    global familyname
+    if intelligence == "beast":
+        partOne = choose("beast_names",raw_dict)
+        partTwo = choose("beast_names_suffix",raw_dict)
+        final = partOne+partTwo
+        final = final.lower()
+        final = final.title()
+    if intelligence == "hybrid":
+        if gender == "female":
+            partOne = choose("human_names_female",raw_dict)
+        elif gender == "male":
+            partOne = choose("human_names_male",raw_dict)
+        partTwo = choose("beast_names_suffix",raw_dict)
+        final = partOne+"'"+partTwo
+        final = final.lower()
+        final = final.title()
+    if intelligence == "human":
+        if gender == "female":
+            partOne = choose("human_names_female",raw_dict)
+        elif gender == "male":
+            partOne = choose("human_names_male",raw_dict)
+        partTwo = choose("human_last_names",raw_dict)
+        if family != "none":
+            partTwo = family
+        else:
+            familyname = partTwo
+        final = partOne+" "+partTwo
+        final = final.lower()
+        final = final.title()
+    if intelligence == "supernatural":
+        partOne   = choose("super_first_names",raw_dict)
+        partTwo   = choose("super_mid_names",raw_dict)
+        partThree = choose("super_seperators",raw_dict)
+        partFour  = choose("super_last_prefix",raw_dict)
+        partFive  = choose("super_last_suffix",raw_dict)
+        if family != "none":
+            partFour = family
+            partFive = ""
+        else:
+            familyname = partFour+partFive
+
+        final = partOne+partThree+partTwo+" "+partFour+partFive
+        final = final.lower()
+        final = final.title()
+
+
+    return final
+
+def generateFamily(origin,intelligence,transmission,gender,familyname):
+    family = ""
+    mate = ""
+    mateOrigin = ""
+    family = ""
+    children = ""
+    if intelligence == "beast" and transmission != "none" or origin in nontf:
+        if random.randint(1,1) == 1 and "search for a mate" not in activities:
+
+            if gender == "male":
+                mate = nameGen(intelligence,"female",familyname)
+            elif gender == "female":
+                mate = nameGen(intelligence,"male",familyname)
+            mateOrigin = transmission
+            #######
+
+            childrenNo = random.randint(0,4)
+            children = []
+            for i in range(childrenNo):
+                children.append(nameGen(intelligence,choose("sex",raw_dict),familyname))
+        else:
+            family = "none"
+    if intelligence == "hybrid" and transmission != "none" or origin in nontf:
+        if random.randint(1,1) == 1 and "search for a mate" not in activities:
+
+            if gender == "male":
+                mate = nameGen(intelligence,"female",familyname)
+            elif gender == "female":
+                mate = nameGen(intelligence,"male",familyname)
+            mateOrigin = transmission
+            #######
+
+            childrenNo = random.randint(0,3)
+            children = []
+            for i in range(childrenNo):
+                children.append(nameGen(intelligence,choose("sex",raw_dict),familyname))
+        else:
+            family = "none"
+
+    if intelligence == "human" and transmission != "none" or origin in nontf:
+        if random.randint(1,1) == 1 and "search for someone crazy enough to like you" not in activities:
+            if gender == "male":
+                mate = nameGen(intelligence,"female",familyname)
+            elif gender == "female":
+                mate = nameGen(intelligence,"male",familyname)
+            mateOrigin = transmission
+            #######
+
+            childrenNo = random.randint(0,2)
+            children = []
+            for i in range(childrenNo):
+                children.append(nameGen(intelligence,choose("sex",raw_dict),familyname))
+        else:
+            family = "none"
+    if intelligence == "supernatural" and transmission != "none" or origin in nontf:
+        if random.randint(1,1) == 1:
+            if gender == "male":
+                mate = nameGen(intelligence,"female",familyname)
+            elif gender == "female":
+                mate = nameGen(intelligence,"male",familyname)
+            mateOrigin = transmission
+            #######
+
+            childrenNo = random.randint(0,1)
+            children = []
+            for i in range(childrenNo):
+                children.append(nameGen(intelligence,choose("sex",raw_dict),familyname))
+        else:
+            family = "none"
+    else:
+        family = "none"
+
+    if mateOrigin == "none" or mateOrigin == "":
+        mateOrigin = "indeterminate"
+
+    return mate, mateOrigin, children
+
+
+
+def printOut(quantity):
+
+    intelligence = choose("anthroscale",raw_dict)
+    disposition = ""
+    for i in range(quantity):
+        part = choose("body_parts",raw_dict)
+        while part in parts:
+            part = choose("body_parts",raw_dict)
+        parts.append(part)
+
+    for i in range(quantity+1):
+        animal = choose("animals",raw_dict)
+        while animal in animals:
+            animal = choose("animals",raw_dict)
+        animals.append(animal)
+
+    if intelligence != "beast":
+        for i in range(1):
+            animal = choose("animals",raw_dict)
+            animals.append(animal)
+
+    for i in range(3):
+
+
+        if intelligence == "beast":
+            act = choose("beast_activities",raw_dict)
+            while act in activities:
+                act = choose("beast_activities",raw_dict)
+            activities.append(act)
+            averageAge = 8
+            disposition = choose("animal_dispositions",raw_dict)
+
+        elif intelligence == "hybrid":
+            act = choose("hybrid_activities",raw_dict)
+            while act in activities:
+                act = choose("hybrid_activities",raw_dict)
+            activities.append(act)
+            averageAge = 15
+            disposition = choose("animal_dispositions",raw_dict)
+
+        elif intelligence == "human":
+            if random.randint(0,1) == 0:
+                act = choose("human_activities_withdrawn",raw_dict)
+                while act in activities:
+                    act = choose("human_activities_withdrawn",raw_dict)
+                activities.append(act)
+                averageAge = 30
+                disposition = "withdrawn"
+            else:
+                act = choose("human_activities_uncaring",raw_dict)
+                while act in activities:
+                    act = choose("human_activities_uncaring",raw_dict)
+                activities.append(act)
+                averageAge = 30
+                disposition = "friendly"
+
+
+
+        elif intelligence == "supernatural":
+            act = choose("supernatural_activities",raw_dict)
+            while act in activities:
+                act = choose("supernatural_activities",raw_dict)
+            activities.append(act)
+            averageAge = 120
+            disposition = "changing"
+
+    furLocations = []
+    furs = choose("fur_locations",raw_dict)
+    for i in range(random.randrange(1,5)):
+        while furs in furLocations:
+            furs = choose("fur_locations",raw_dict)
+        furLocations.append(furs)
+    if "none" in furLocations:
+        furLocations = ["none"]
+    elif "entire body" in furLocations:
+        furLocations = ["entire body"]
+
+    age = averageAge + random.randrange(-int(round((averageAge/10)*5)),int(round((averageAge/10)*5)))
+
+    lifeStage = ""
+    if intelligence == "beast":
+        if age < 3:
+            lifeStage = "young"
+        elif age < 5:
+            lifeStage = "teenager"
+        elif age < 8:
+            lifeStage = "adult"
+        elif age <= 13:
+            lifeStage = "middle-aged"
+        elif age > 13:
+            lifeStage = "elderly"
+    elif intelligence == "hybrid":
+        if age < 6:
+            lifeStage = "young"
+        elif age < 10:
+            lifeStage = "teenager"
+        elif age < 16:
+            lifeStage = "adult"
+        elif age <= 20:
+            lifeStage = "middle-aged"
+        elif age > 20:
+            lifeStage = "elderly"
+    elif intelligence == "human":
+        if age < 13:
+            lifeStage = "young"
+        elif age < 20:
+            lifeStage = "teenager"
+        elif age < 40:
+            lifeStage = "adult"
+        elif age <= 50:
+            lifeStage = "middle-aged"
+        elif age > 50:
+            lifeStage = "elderly"
+    elif intelligence == "supernatural":
+        if age < 30:
+            lifeStage = "young"
+        elif age < 60:
+            lifeStage = "teenaged"
+        elif age < 160:
+            lifeStage = "adult"
+        elif age <= 180:
+            lifeStage = "middle-aged"
+        elif age > 180:
+            lifeStage = "elderly"
+    else:
+        lifeStage = "bobRoss"
+
+
+
+    gender = choose("sex",raw_dict)
+
+    origin = choose("origins",raw_dict)
+    originalAnimal = choose("animals+human",raw_dict)
+    fillerAnimal = choose("animals",raw_dict)
+
+    name = nameGen(intelligence,gender,"none")
+
+    transmission = choose("transmission",raw_dict)
+
+    if lifeStage != "young":
+        family = generateFamily(origin,intelligence,transmission,gender,familyname)
+    else:
+        family = 'none'
+
+    if '' in family:
+        family = "none"
+
+    if origin in synth:
+        robotParts,robotype = generateRobotParts()
+
+        robotstring = ["Your synthetic parts include "]
+        try:
+            for i in range(len(parts)-1):
+                robotstring.append("".join(\
+                [robotype[i]," ",robotParts[i],", "]\
+                ))
+        except:
+            pass
+        robotstring.append("".join(\
+            [robotype[-1]," ",robotParts[-1],"."]\
+            ))
+
+        if "entire body" in robotParts:
+            robotstring = "You entire body is mechanised."
+
+
+    body_type = choose("body_type",raw_dict)
+    body_form = choose("body_form",raw_dict)
+    childPronoun = choose("family_beast",raw_dict)
+    if intelligence == "beast" or intelligence == "hybrid":
+        emotion = choose("beast_emotions",raw_dict)
+    else:
+        emotion = choose("emotions",raw_dict)
+    freq = choose("frequency",raw_dict)
+
+    joinstring = []
+    try:
+        for i in range(len(parts)-1):
+            joinstring.append("".join(\
+            [parts[i]," of a ",animals[i],", "]\
+            ))
+    except:
+        pass
+    joinstring.append("".join(\
+        [parts[-1]," of a ",animals[-1],"."]\
+        ))
+
+
+    joinstring = "".join(joinstring)
+
+    print("You call yourself",name+", and are",gender+".")
+    print("You have the",joinstring)
+
+    furDensity = choose("fur_density",raw_dict)
+    furAction = choose("fur_action",raw_dict)
+
+    if furLocations == ["none"]:
+        joinstring = "You are entirely bare of fur."
+    elif furLocations == ["entire body"]:
+        joinstring = ["Your whole body is covered in a ",furDensity+" of fur."]
+    else:
+        joinstring = ["You have a ",furDensity," of fur ",furAction," your "]
+        try:
+            for i in range(len(furLocations)-1):
+                joinstring.append("".join(\
+                [furLocations[i],", "]\
+                ))
+        except:
+            pass
+        joinstring.append("".join(\
+                [furLocations[-1],"."]\
+                ))
+
+    if origin in synth:
+        if random.randrange(1,2) == 1:
+            furplace = ["Your body is plated in metal."]
+
+    tfPain = False
+    while tfPain == False:
+        tfPain = choose("transformation_pain",raw_dict)
+    tfTime = choose("time_scale",raw_dict)
+    tfSpecific = False
+    trigger = False
+    mateOrigin = "finding a"
+    if transmission == "biological":
+        tfSpecific = choose("bio_transmission",raw_dict)
+        mateOrigin = choose("mate_origins",raw_dict)
+    elif transmission == "magical":
+        tfSpecific = choose("magical_transmission",raw_dict)
+        mateOrigin = choose("mate_origins",raw_dict)
+    else:
+        tfSpecific = choose("bio_transmission"+"magical_transmission",raw_dict)
+    if random.randrange(1,5) == 1:
+        trigger = choose("trigger",raw_dict)
+
+
+
+
+
+    furplace = "".join(joinstring)
+
+    ###CHILDREN
+
+    childstring = []
+    if family != "none":
+
+        try:
+            for i in range(len(family[2])-1):
+                childstring.append("".join(\
+                [family[2][i]+", "]\
+                ))
+            childstring.append("".join(\
+            [family[2][-1]]\
+            ))
+        except:
+            pass
+
+
+    childstring = "".join(childstring)
+
+
+
+    x = ""
+    if origin[0] in["a","e","i","o","u"]:
+        x = "n"
+
+    y = ""
+    if len(family[2]) > 1:
+        y = "s"
+
+
+    print("Everywhere else, you are a",fillerAnimal+". You're",age,"years old (which puts you at '"+lifeStage+"' in your lifetime), quite",body_type+", and are",body_form+".")
+    print(furplace)
+    if origin not in nontf:
+        print("You are a{}".format(x),origin+", but originally you were a",originalAnimal+", and are about as intelligent as a",intelligence+".")
+        if transmission == "none":
+            pass
+        else:
+            if transmission == "natural":
+                print("Other beings of your type exist naturally.")
+            else:
+                print("You can turn others into your form by",tfSpecific,"This transformation involves",tfPain,"pain, and takes",tfTime)
+    else:
+        print("You are a{}".format(x),origin+", and are about as intelligent as a",intelligence+".")
+    if origin in synth:
+        robotstring = "".join(robotstring)
+        print(robotstring)
+    if trigger != False:
+        print("You're not always in your chimeral form, only",trigger)
+    if trigger == False:
+        print("During the day, you",activities[0]+",",activities[1]+" and",activities[2]+".")
+    else:
+        print("While in your chimeral form, you",activities[0]+",",activities[1]+" and",activities[2]+".")
+    if disposition != "":
+        print("You're of a",disposition,"disposition, and feel",emotion,freq+".")
+    if family != "none" and transmission != "none":
+        if intelligence == "beast" or intelligence == "hybrid":
+            if gender == "male":
+                if len(family[2]) != 0:
+                    print("After",mateOrigin,"mate,",family[0]+", she bore you",len(family[2]),childPronoun+"{}".format(y)+", who you named",childstring+".")
+                else:
+                    print("Your have a mate named",family[0]+", who is",family[1],"in origin.")
+            elif gender == "female":
+                if len(family[2]) != 0:
+                    print("After",mateOrigin,"mate,",family[0]+", you bore him",len(family[2]),childPronoun+"{}".format(y)+", who you named",childstring+".")
+                else:
+                    print("Your have a mate named",family[0]+", who is",family[1],"in origin.")
+        elif intelligence == "human":
+            if gender == "male":
+                if len(family[2]) != 0:
+                    print("After",mateOrigin,"wife,",family[0]+", she gave birth to",len(family[2]),childPronoun+"{}".format(y)+", who you named",childstring+".")
+                else:
+                    print("Your have a wife named",family[0]+", who is",family[1],"in origin.")
+            elif gender == "female":
+                if len(family[2]) != 0:
+                    print("After",mateOrigin,"husband,",family[0]+", you gave birth to",len(family[2]),childPronoun+"{}".format(y)+", who you named",childstring+".")  #sorry to trigger you guys
+                else:
+                    print("Your have a husband named",family[0]+", who is",family[1],"in origin.")
+        elif intelligence == "supernatural":
+            if gender == "male":
+                if len(family[2]) != 0:
+                    print("After",mateOrigin,"partner,",family[0]+", she gave birth to",len(family[2]),childPronoun+"{}".format(y)+", who you named",childstring+".")
+                else:
+                    print("Your have a partner named",family[0]+", who is",family[1],"in origin.")
+            elif gender == "female":
+                if len(family[2]) != 0:
+                    print("After",mateOrigin,"partner,",family[0]+", you gave birth to",len(family[2]),childPronoun+"{}".format(y)+", who you named",childstring+".")  #sorry to trigger you guys
+                else:
+                    print("Your have a partner named",family[0]+", who is",family[1],"in origin.")
+
+
+#TODO: Height
+#      Synthetic Abilities
+#      Abilities in general
+
+
+while True:
+    if input("Press enter for a new chimera.") == "":
+        print("\n")
+        printOut(3)
+        already_changed = []
+        parts = []
+        animals = []
+        activities = []
+        furLocations = []
+        robotstring = ""
+        print("\n")
